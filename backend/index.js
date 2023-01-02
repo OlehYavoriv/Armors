402b4d95c9e7d1bdc502043a5b2b51ad7c8b327e
OlehYavoriv <oleh.yavoriv@gmail.com>
2023-01-02 15:54:35 +0200

    Modified

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,22 @@ require("dotenv").config();
 const express = require("express");
 const sequelize = require("./db");
 const cors = require("cors");
+const fileUpload = require("express-fileupload");
 const models = require("./models/models");
 const router = require("./routes/index");
+const errorHandler = require("./middleware/ErrorHandlingMiddleware");
+const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "static")));
+app.use(fileUpload({}));
 app.use("/api", router);
+
+app.use(errorHandler);
 
 const start = async () => {
   try {
