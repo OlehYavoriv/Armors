367f03434a30587912b42541f518ddaa678b51d5
OlehYavoriv <oleh.yavoriv@gmail.com>
2023-01-02 12:10:37 +0200

    Connect to DB

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
 require("dotenv").config();
 const express = require("express");
+const sequelize = require("./db");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
