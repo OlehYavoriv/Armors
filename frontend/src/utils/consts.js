a72b37651ce0831b8631bc77e4280ccbb5543cb2
OlehYavoriv <oleh.yavoriv@gmail.com>
2023-01-08 22:51:56 +0200

    Add constants for routing

---
+++ frontend/src/utils/consts.js
@@ -0,0 +1,9 @@
+export const ADMIN_ROUTE = "/admin";
+export const LOGIN_ROUTE = "/login";
+export const REGISTRATION_ROUTE = "/registration";
+export const SHOP_ROUTE = "/";
+export const BASKET_ROUTE = "/basket";
+export const PRODUCT_ROUTE = "/product";
+export const ABOUT_ROUTE = "/about";
+export const FAQ_ROUTE = "faq";
+export const CONTACT_ROUTE = "/contacts";
