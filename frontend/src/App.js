7c92fdc1b3d45ffc6cb3a8b739532d6cee3fb500
OlehYavoriv <oleh.yavoriv@gmail.com>
2023-01-10 13:21:59 +0200

    Create structure site

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,37 @@
-const App = () => {
+import { observer } from "mobx-react-lite";
+import { useContext, useEffect, useState } from "react";
+import { Spinner } from "react-bootstrap";
+import { BrowserRouter } from "react-router-dom";
+import { Context } from ".";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import AppRouter from "./components/Routing";
+import { check } from "./http/userAPI";
+
+const App = observer(() => {
+  const { user } = useContext(Context);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    check()
+      .then((data) => {
+        user.setUser(true);
+        user.setIsAuth(true);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return <Spinner animation={"grow"} />;
+  }
+
   return (
-    <div>
-      <p>Armors</p>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <AppRouter />
+      <Footer />
+    </BrowserRouter>
   );
-};
+});
 
 export default App;
